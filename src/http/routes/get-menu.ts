c09953575ec5e04bff48b35b9194cacbff61892e
gbmoraes-dev <gbmoraes.dev@gmail.com>
2024-10-16 23:03:51 -0300

    refactor: update get menu route to use dynamic restaurantId parameter and pagination

--- src/http/routes/get-menu.ts
+++ src/http/routes/get-menu.ts
@@ -1,19 +1,36 @@
-import Elysia from 'elysia'
+import Elysia, { t } from 'elysia'
 
 import { db } from '@/db/connection'
 
-import { authenticate } from '@/http/authenticate'
+import { z } from 'zod'
 
-export const getMenu = new Elysia()
-	.use(authenticate)
-	.get('/menu', async ({ getManagedRestaurantId }) => {
-		const restaurantId = await getManagedRestaurantId()
+export const getMenu = new Elysia().get(
+	'/restaurants/:restaurantId/menu',
+	async ({ params, query }) => {
+		const { restaurantId } = params
+
+		const { pageIndex } = z
+			.object({
+				pageIndex: z.coerce.number().default(0),
+			})
+			.parse(query)
 
 		const menu = await db.query.products.findMany({
+			offset: pageIndex * 10,
+			limit: 10,
 			where(fields, { eq }) {
 				return eq(fields.restaurantId, restaurantId)
 			},
 		})
 
 		return menu
-	})
+	},
+	{
+		params: t.Object({
+			restaurantId: t.String(),
+		}),
+		query: t.Object({
+			pageIndex: t.Numeric({ minimum: 0 }),
+		}),
+	},
+)
